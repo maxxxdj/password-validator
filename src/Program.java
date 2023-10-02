639b3c92fa45506b3c65fa062e9f1b9775fdd612
maxxx <martinmiloshev@gmail.com>
2023-10-02 22:18:14 +0300

    Updated switch cases

--- src/Program.java
+++ src/Program.java
@@ -1,20 +1,19 @@
 public class Program {
     public static void main(String[] args) {
 
-
         /** Testing when password has only letters and is less than 10 characters. */
-        String strPass = "abv";
+        var strPass = "abv";
         System.out.println(validatePassword(strPass));
 
-        /** Testing when password has only digits. */
-        int intPass = 12345;
+        /** Testing when password has only digits (works with other numeric values). */
+        var intPass = 12345;
         System.out.println(validatePassword(intPass));
 
         /** Testing when password is null. */
         System.out.println(validatePassword(null));
 
         /** Testing when password is valid! */
-        Object validPasswordAsObj = "sad21cX12aaaDDc";
+        var validPasswordAsObj = "sad21cX12aaaDDc";
         System.out.println(validatePassword(validPasswordAsObj));
 
     }
@@ -22,10 +21,9 @@ public class Program {
     private static String validatePassword(final Object password) {
         return switch (password) {
             case null -> "Password cannot be empty!";
-            case Integer i -> "Password cannot consist only of numbers!";
-            case String s when s.length() > 10 && s.matches(".*[a-z]|.*[A-Z]|.*[0-9]")
-                    -> "Password is valid!";
-            default -> "Invalid password!";
+            case Number i -> "Password cannot consist only of numbers!";
+            case String s when s.length() > 10 && s.matches(".*[a-z]|.*[A-Z]|.*[0-9]") -> "Password is valid!";
+            default -> "Invalid password! \n" + "Password must include an uppercase, lowercase, and a number.";
         };
     }
 }
